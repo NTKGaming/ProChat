1d12a81d03d49277f3057ed2698c161198304695
NTKGaming <102576372+NTKGaming@users.noreply.github.com>
2024-10-19 10:54:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const server= http.createServer(app);
 const {Server} =require('socket.io');
 const io = new Server(server);
 
-app.get('/', (req,res) =>{
-    res.sendFile(__dirname + '/index.html')
 
-});
+app.use(express.static('public'));
+
 io.on('connection', (socket) =>{
     console.log('user connected');
     socket.on('on-chat', data =>{
@@ -19,7 +18,7 @@ io.on('connection', (socket) =>{
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () =>{
-    console.log('listening on port 3000');
+    console.log(`listening on port ${port}`);
 
 
 });
